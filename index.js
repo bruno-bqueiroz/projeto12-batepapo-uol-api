c52a5720d9397c0b7c429529b5035db94bf53b48
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-09-01 15:09:38 -0400

    post e get de participantes salvando no mongo

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 import express from "express";
 import cors from 'cors';
 import dayjs from 'dayjs'
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-let cadastro = [
-    {
-        name: "João"
-    },
-    {
-        name: "Pedro"
-    },
-    {
-        name: "Jose"
-    }
-    ];
-let mensagem = [
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
-];
+let db;
+mongoClient.connect().then(()=>{
+    db = mongoClient.db('test');
+})
+    
+let tem = undefined;
 
     server.post('/participants', (req, res) =>{
-        const name = req.body.name;
-        if (name === ""){
+        const nome = req.body.name;
+       
+        db.collection('cadastrados').findOne({ name: nome }).then((data)=>{
+            console.log(data);
+            tem = data;
+        })
+       
+         if (nome === ""){
             return res.sendStatus(422);
-        }if(cadastro.find((value) => value.name === name)){
+        } else if(tem !== null){
             return res.sendStatus(409);
-        }
-        cadastro = [...cadastro, {name: name, lastStatus: Date.now()}];
-        mensagem = [...mensagem, {from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss')}]
-        res.sendStatus(201);
+        } 
+  
+        db.collection('cadastrados').insertOne({name: nome, lastStatus: Date.now()})
+        db.collection('mensagens').insertOne({from: nome, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss')})
+            res.sendStatus(201);
     });
 
-    server.get('/participants', (req, res) => res.send(cadastro));
+    server.get('/participants', (req, res) => {
+        db.collection('cadastrados').find().toArray().then(data =>{
+            console.log(data)
+            res.send(data)
+        })
+    });
 
     server.post('/messages', (req, res)=>{
         const {to, text, type} = req.body;
@@ -45,9 +54,7 @@ let mensagem = [
         || !cadastro.find((value) => value.name === from)){
             return res.sendStatus(422);
         }
-
         res.send(201);
     })
-
 
 server.listen(5000, () => console.log('listen on port 5000'));
