649b8294a41938153724fd6b2308bc1f7bfe72b9
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-09-05 15:48:20 -0400

    acertando detalhes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const nomeSchema = joi.object({
 const mensagensSchema = joi.object({
     to: joi.string().required(),
     text: joi.string().required(),
-    type: joi.string().valid('message').valid('private_message').required()
+    type: joi.string().valid('status').valid('message').valid('private_message').required()
 })
 
 
@@ -72,38 +72,36 @@ const mensagensSchema = joi.object({
             res.sendStatus(500);
         }
 
-        
-        
     });
 // ENVIAR MENSAGENS
     server.post('/messages', async (req, res)=>{
         const {to, text, type} = req.body;
         const from = req.headers.user;
-        console.log(req.headers.user);
+        console.log(to);
 
         const validation = mensagensSchema.validate(req.body);
         if(validation.error){
             return res.status(422).send(validation.error.message)
         }
-        
         try {
             const response = await db.collection('cadastrados').findOne({ name: from });
             if(response.name === null){
                 return res.sendStatus(422);
             }
+            try {
+                
+const retorno = await db.collection('mensagens').insertOne({from: from, to: to, text: text, type: type, time: dayjs().format('HH:mm:ss')})
+            console.log(retorno);
+            res.status(201).send(retorno);
+            } catch (error) {
+                console.log(error);
+                res.sendStatus(500);
+            }
         } catch (error) {
             console.log(error);
             res.sendStatus(500);
         }
-
-        try {
-            const retorno = await db.collection('mensagens').insertOne({from: from, to: to, text: text, type: type, time: dayjs().format('HH:mm:ss')})
-        console.log(retorno);
-        res.sendStatus(201);
-        } catch (error) {
-            console.log(error);
-            res.sendStatus(500);
-        }
+        
     })
 // RECEBER MENSAGENS
     server.get('/messages', async (req, res) =>{
@@ -112,11 +110,11 @@ const mensagensSchema = joi.object({
 
         try {
             if (limit){
-                const resposta = await db.collection('mensagens').find({$or: [{ from: usuario } , { to: 'todos' } , {to: usuario }]}).limit(parseInt(limit)).toArray();
+                const resposta = await db.collection('mensagens').find({$or: [{ from: usuario }, {to: usuario }, { to: 'Todos' }]} ).limit(parseInt(limit)).toArray();
                 return res.send(resposta);
             } 
 
-            const resposta = await db.collection('mensagens').find({$or: [{ from: usuario } , { to: 'todos' } , {to: usuario }]}).toArray();
+            const resposta = await db.collection('mensagens').find({$or: [{ from: usuario }, {to: usuario }, { to: 'Todos' } ]}).toArray();
             return res.send(resposta);
             
         } catch (error) {
@@ -127,23 +125,35 @@ const mensagensSchema = joi.object({
 
     server.post('/status', async (req, res)=>{
         const usuario = req.headers.user;
-        console.log(usuario);
-        try {
+/*         console.log(usuario);
+ */        try {
             const resposta = await db.collection('cadastrados').findOne({ name: usuario});
             if(!resposta){
                 return res.sendStatus(404);
             }
-
             const atualizar = await db.collection('cadastrados').updateOne({ name: usuario}, {$set:{lastStatus: Date.now()}});
 
-            console.log(atualizar);
-            
+/*             console.log(atualizar);
+ */            
             res.sendStatus(200)
-
         } catch (error) {
             console.log(error);
             res.sendStatus(500);
         }
     })
+
+    setInterval ( async ()=>{
+        const response = await db.collection('cadastrados').find().toArray();
+        const off = response.filter((value) => value.lastStatus < (Date.now()-10000) )    
+
+        console.log(off);
+
+        if(off){
+            off.map(async (value) =>{
+                await db.collection('cadastrados').deleteOne({ name: value.name});
+                await db.collection('mensagens').insertOne({from: value.name, to: 'Todos', text: 'sai da sala...', type: 'status', time: dayjs().format('HH:mm:ss') })
+            })    
+        }
+    }, 15000);
 
 server.listen(5000, () => console.log('listen on port 5000'));
